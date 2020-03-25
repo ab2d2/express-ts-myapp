9db8ee2cf63c5ce8de4723e440229f4704ca1c04
ab2d2 <abdul.rahimi@capgemini.com>
2020-03-25 19:16:05 +0000

    use async await

--- server/api/controllers/examples/controller.ts
+++ server/api/controllers/examples/controller.ts
@@ -7,20 +7,20 @@ export class Controller {
   }
 
   byId(req: Request, res: Response): void {
-    const id = Number.parseInt(req.params['id'])
+    const id = Number.parseInt(req.params['id']);
     ExamplesService.byId(id).then(r => {
       if (r) res.json(r);
       else res.status(404).end();
     });
   }
 
-  create(req: Request, res: Response): void {
-    ExamplesService.create(req.body.name).then(r =>
-      res
-        .status(201)
-        .location(`/api/v1/examples/${r.id}`)
-        .json(r),
-    );
+  async create(req: Request, res: Response): Promise<Response> {
+    const r = await ExamplesService.create(req.body.name);
+
+    return res
+      .status(201)
+      .location(`/api/v1/examples/${r.id}`)
+      .json(r);
   }
 }
 export default new Controller();
