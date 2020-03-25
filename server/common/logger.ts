fe231470186bb33c67d39b64cdab756c762c07d0
ab2d2 <abdul.rahimi@capgemini.com>
2020-03-25 19:18:46 +0000

    turn down logging in tests

--- server/common/logger.ts
+++ server/common/logger.ts
@@ -2,7 +2,10 @@ import pino from 'pino';
 
 const l = pino({
   name: process.env.APP_ID,
-  level: process.env.LOG_LEVEL,
+  level:
+    process.env.NODE_ENV === 'test'
+      ? process.env.LOG_LEVEL_TEST
+      : process.env.LOG_LEVEL,
 });
 
 export default l;
