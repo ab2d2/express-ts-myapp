5b0474d95747f3ce28f8c97bd2a1832ef8d5b63e
ab2d2 <abdul.rahimi@capgemini.com>
2020-03-25 20:15:01 +0000

    run lint:fix

--- server/index.ts
+++ server/index.ts
@@ -2,5 +2,6 @@ import './common/env';
 import Server from './common/server';
 import routes from './routes';
 
-const port = process.env.NODE_ENV === 'test' ? 3001 : parseInt(process.env.PORT);
+const port =
+  process.env.NODE_ENV === 'test' ? 3001 : parseInt(process.env.PORT);
 export default new Server().router(routes).listen(port);
