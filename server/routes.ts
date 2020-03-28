1b25af14649537c2fd92deb7951dcae53b1e1730
ab2d2 <abdul.rahimi@capgemini.com>
2020-03-28 16:55:43 +0000

    add new route for Users

--- server/routes.ts
+++ server/routes.ts
@@ -1,5 +1,8 @@
 import { Application } from 'express'
 import examplesRouter from './api/controllers/examples/router'
+import authRouter from './api/controllers/auth/router'
+
 export default function routes(app: Application): void {
   app.use('/api/v1/testing', examplesRouter)
+  app.use('/api/v1/token', authRouter)
 }
